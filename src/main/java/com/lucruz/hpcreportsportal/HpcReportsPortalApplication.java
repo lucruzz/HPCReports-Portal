9f5300eb8838bfe89cfdff1cc14d2dbf71b23f80
lucruzz <lucascruzlcs@gmail.com>
2026-08-27 08:45:22 -0300

    first commit

---
+++ src/main/java/com/lucruz/hpcreportsportal/HpcReportsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.lucruz.hpcreportsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcReportsPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcReportsPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucruz/hpcreportsportal/HpcReportsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucruz.hpcreportsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcReportsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
